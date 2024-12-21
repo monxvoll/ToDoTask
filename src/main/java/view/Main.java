b6ead1009492c7d701f134bac911650b54b8df38
monx.voll <stedy3348112688@gmail.com>
2024-12-21 12:25:29 -0500

    Pequeño menu de prueba

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,19 +1,34 @@
 package view;
 
+import controller.LoginController;
 import controller.RegisterController;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // Inicializar Firebase
         FirebaseInitializer.initialize();
         RegisterController registerController = new RegisterController();
+        LoginController loginController = new LoginController();
 
-        // Registrar usuarios (ejemplo de prueba)
-        for (int i = 0; i < 5; i++) {
-            registerController.registerUser();
+        while (true) {
+        System.out.println("1.Iniciar Sesion");
+        System.out.println("2.Registrarse");
+        String opt = scanner.nextLine();
+            switch (opt) {
+                case "1":
+                    loginController.login();
+                    break;
+                case "2":
+                    registerController.registerUser();
+                    break;
+                default:
+                    System.err.println("Digite una opcion valida");
+            }
         }
 
-        // Cerrar el scanner una vez que todos los registros hayan terminado
-        registerController.closeScanner();
+
     }
 }
