09516e891865289c30ed35d555000c91671972ce
monx.voll <stedy3348112688@gmail.com>
2024-12-24 19:14:33 -0500

    Se añade opcion para probar la funcion de añadir notas y la del cierre de sesion

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -2,11 +2,15 @@ package view;
 
 import controller.auth.LoginController;
 import controller.auth.RegisterController;
+import controller.note.CreateController;
 import model.entities.User;
-
 import java.util.Scanner;
 
+
 public class Main {
+
+   private static CreateController createController ;
+   private static boolean flag = true;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Inicializar Firebase
@@ -14,15 +18,17 @@ public class Main {
         RegisterController registerController = new RegisterController();
         LoginController loginController = new LoginController();
 
-        while (true) {
+
+
+        while (flag) {
         System.out.println("1.Iniciar Sesion");
         System.out.println("2.Registrarse");
         String opt = scanner.nextLine();
             switch (opt) {
                 case "1":
                     if(loginController.login()){
-                        showNotesMenu(loginController.getActualUser());
-                        System.exit(0);
+                        setFlagFalse();
+                        showNotesMenu(loginController.getActualUser(),scanner);
                     }
                     break;
                 case "2":
@@ -32,16 +38,44 @@ public class Main {
                     System.err.println("Digite una opcion valida");
             }
         }
-
-
     }
 
-    public static void showNotesMenu(User user){
-       //En construccion
-        executeOption(0,user);
+    public static void showNotesMenu(User user,Scanner scanner){
+        while (!flag) {
+            System.out.println("1. Crear una nota nueva");
+            System.out.println("2. Ver notas actuales");
+            System.out.println("3. Editar una nota");
+            System.out.println("4. Eliminar una nota");
+            System.out.println("5. Cerrar Sesion");
+            String opt = scanner.nextLine();
+            executeOption(opt, user);
+        }
     }
 
-    private static void executeOption(int opt,User user){
-        //En construccion
+    private static void executeOption(String opt,User user){
+        switch (opt){
+            case "1":
+                createController = new CreateController();
+                createController.createNote(user);
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                setFlagTrue();
+                break;
+            default:
+                System.out.println("Por favor digite una opcion valida");
+        }
+    }
+
+    public static void setFlagTrue(){
+        flag = true;
+    }
+    public static void setFlagFalse(){
+        flag = false;
     }
 }
