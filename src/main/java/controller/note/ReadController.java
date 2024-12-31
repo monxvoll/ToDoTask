17c70fb308e40daa9466deb577798ef21745860b
monx.voll <stedy3348112688@gmail.com>
2024-12-31 11:05:44 -0500

    Añadido controlador para la lectura de notas

--- src/main/java/controller/note/ReadController.java
+++ src/main/java/controller/note/ReadController.java
@@ -1,4 +1,16 @@
 package controller.note;
 
+import model.entities.User;
+import model.noteLogic.Read;
+
 public class ReadController {
+    private Read read;
+
+    public ReadController (){
+        this.read = new Read();
+    }
+
+    public void readNotes(User user){
+        read.readNotes(user);
+    }
 }
